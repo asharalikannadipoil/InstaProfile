23ef44e08154f92e5f770a599170e359823fcdfa
Asharali V U <worldotapss@gmail.com>
2025-04-25 21:38:01 +0530

    New launcher cons are added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,15 +58,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
-    implementation("androidx.navigation:navigation-compose:2.8.9")
-    implementation("io.coil-kt:coil-compose:2.5.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation (libs.accompanist.systemuicontroller)
 }
 
 kapt {
